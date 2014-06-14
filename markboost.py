f24615eb78cfb6fc9f79e88c36d8a0a210ce3541
nick-hu <nicholas.squishy@gmail.com>
2014-06-14 13:44:08 -0700

    markboost: correction

--- markboost.py
+++ markboost.py
@@ -1,14 +1,15 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
-# SECRET:
-# Mr. Arkiletian's mark-boosting function
 
 def newmark(oldmark):
+
+    """Mr. Arkiletian's mark-boosting function"""
+
     return 10 * round(oldmark ** 0.5, 0)  # 10 times square root of old mark
 
 
 while True:
     old = raw_input()
-    if old == '':
+    if old == "":
         break
-    print newmark(old), '\n'
+    print newmark(float(old)), '\n'
